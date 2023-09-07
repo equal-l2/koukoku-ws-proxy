9590bc4f501606dd6555331c15a8206ae03062f5
equal-l2 <eng.equall2@gmail.com>
2023-09-07 23:20:17 +0900

    色と強制改行に対応

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Koukoku WS Proxy
-某公告Telnetサーバ用のWebSocketプロクシです。  
-デフォルトでは `ws://localhost:8080/ws` にWSエンドポイントを立てます。  
 
-公告→WS：放話のみを取り出してWebSocketに流します。  
-WS→公告：受け取ったMessageをそのまま公告サーバに流します。  
+某公告 Telnet サーバ用の WebSocket プロクシです。  
+デフォルトでは `ws://localhost:8080/ws` に WS エンドポイントを立てます。
+
+公告 →WS：放話のみを取り出して WebSocket に流します。  
+WS→ 公告：受け取った Message をそのまま公告サーバに流します。
 
 # 使い道
-1. Webクライアントのバックエンドとして使う
-2. websocatなどを使ってCLIから叩く
+
+1. Web クライアントのバックエンドとして使う
+2. websocat などを使って CLI から叩く
+
+# TODO
+
+- 演説対応
+- エスケープシーケンスの削除
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use axum::routing::get;
 use axum::Router;
 use futures::sink::SinkExt;
 use futures::stream::StreamExt;
+use log::*;
 use tokio::{
     self,
     io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt},
@@ -50,56 +51,125 @@ async fn connect_koukoku() -> anyhow::Result<tokio_rustls::client::TlsStream<tok
 }
 
 async fn ws_handler(sock: WebSocket) {
+    info!("new Websocket connection accepted");
     let stream = match connect_koukoku().await {
         Ok(inner) => inner,
         Err(e) => {
-            eprintln!("INFO: Failed to connect with koukoku: {:?}", e);
+            error!("Failed to connect with koukoku: {:?}", e);
             return;
         }
     };
-    eprintln!("INFO: Connected to koukoku");
+
+    info!("Connected to koukoku");
 
     let (ext_rx, ext_tx) = tokio::io::split(stream);
 
     let (ws_tx, ws_rx) = sock.split();
 
-    let res = tokio::try_join!(connect_read(ext_rx, ws_tx), connect_write(ws_rx, ext_tx));
+    let read_handle = relay_read(ext_rx, ws_tx);
+    let write_handle = relay_write(ws_rx, ext_tx);
+
+    // 片方が終了したら終わる
+    let res = tokio::select! {
+        val = read_handle => {
+            val
+        }
+        val = write_handle => {
+            val
+        }
+    };
+
     if let Err(e) = res {
-        eprintln!("INFO: WebSocket closed: {:?}", e);
+        info!("WebSocket closed with error: {:?}", e);
+    } else {
+        info!("WebSocket closed");
     }
 }
 
 /// 外から読んでWebSocketに流す
-async fn connect_read<F>(ext_rx: impl AsyncRead + Unpin, mut ws_tx: F) -> anyhow::Result<()>
+async fn relay_read<F>(ext_rx: impl AsyncRead + Unpin, mut ws_tx: F) -> anyhow::Result<()>
 where
     F: futures::Sink<Message> + Unpin,
     <F as futures::Sink<axum::extract::ws::Message>>::Error:
         Send + Sync + std::error::Error + 'static,
 {
+    trace!("relay_read");
     let mut reader = tokio::io::BufReader::new(ext_rx);
+
+    //  複数行来る場合があるのでくっつけるためのやつ
+    let mut line_buf = Vec::new();
+
     let mut buf = String::new();
+
+    let mut in_msg = false;
     loop {
         reader.read_line(&mut buf).await?;
 
         // チャットメッセージのみを流す
-        if buf.starts_with(">>") {
-            ws_tx.send(Message::Text(buf.clone())).await?;
+        // TODO: 演説
+        trace!("RECV Content: {}", buf);
+        trace!(
+            "RECV bytes (first 5): {:?}",
+            buf.bytes().collect::<Vec<_>>()
+        );
+
+        let is_start_msg = || {
+            const START_PATTERNS: &[&str] = &[
+                ">>",                       // 通常
+                "\x1b[0m\x1b[1m\x1b[31m>>", // 赤色開始
+                "\x1b[0m\x1b[1m\x1b[32m>>", // 緑色開始(自分の放話)
+            ];
+            START_PATTERNS.iter().any(|s| buf.starts_with(s))
+        };
+        let is_empty_end = || {
+            const END_PATTERNS: &[&str] = &[
+                "\r\n",            // 通常
+                "\x1b[0m\x07\r\n", // 色付き終了
+            ];
+            END_PATTERNS.iter().any(|s| buf.ends_with(s))
+        };
+        if is_start_msg() && !in_msg {
+            debug!("start message");
+            line_buf.push(buf.clone());
+            in_msg = true;
+        } else if in_msg {
+            line_buf.push(buf.clone());
+            if buf.ends_with("<<\r\n") {
+                debug!("end message");
+                in_msg = false;
+            } else if is_empty_end() {
+                // なにかうまくいってないとき
+                // 放話に空行は入らないはずなので終わらせる
+                debug!("abrupt end message");
+                in_msg = false;
+            }
+
+            if !in_msg {
+                // 放話終了
+                let msg = line_buf.join("").replace("\r\n", "");
+                ws_tx.send(Message::Text(msg)).await?;
+                line_buf.clear();
+            }
         }
         buf.clear()
     }
 }
 
 /// WebSocketから読んで外に流す
-async fn connect_write<E>(
+async fn relay_write<E>(
     mut ws_rx: impl futures::Stream<Item = Result<Message, E>> + Unpin,
     mut ext_tx: impl AsyncWrite + Unpin,
 ) -> anyhow::Result<()>
 where
     E: Send + Sync + std::error::Error + 'static,
 {
+    trace!("relay_write");
+    // 公告表示無効化
+    ext_tx.write_all(b"nobody\n").await?;
     while let Some(msg) = ws_rx.next().await {
         let msg = msg?;
         if let Message::Text(s) = msg {
+            debug!("SEND Content: {}", s);
             ext_tx.write_all(s.as_bytes()).await?;
         }
     }
@@ -113,6 +183,7 @@ async fn get_handler(ws: WebSocketUpgrade) -> Response {
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     let app = Router::new().route(PROXY_ENDPOINT, get(get_handler));
 
     let addr = (LISTEN_ADDR, LISTEN_PORT)
@@ -121,7 +192,7 @@ async fn main() {
         .next()
         .expect("should contain an entry");
 
-    println!("Listening on port {}", LISTEN_PORT);
+    eprintln!("Listening on port {}", LISTEN_PORT);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
